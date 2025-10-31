d7bfc42cc5f0426ef90551c17de07d60d726319f
rafli <rafli.4lyfe@gmail.com>
2025-10-31 23:07:05 +0700

    Update label_dict with additional image entries
    Added new image entries to various food labels in the label_dict.

--- utils/labeled manually.py
+++ utils/labeled manually.py
@@ -1,8 +1,9 @@
 label_dict = {
-        "Ayam Bakar": [
+    "Ayam Bakar": [
         'zt93li.jpg', 'ztaz8z.jpg', 'zd4bwe.jpg', 'zgp5q9.jpg', 'ytcwjz.jpg', 'ywtpui.jpg', 'ywx5mg.jpg',
         'z0zm3z.jpg', 'irh22j.jpg', 'ifc83g.jpg', 'hzgnpg.jpg', 'o7ulw5.jpg', 'hop7y8.jpg', 'gr31o3.jpg',
-        'ga7agq.jpg', 'gdl2m4.jpg', 'g3p5t6.jpg', 'g3qflx.jpg', 'd4rzqt.jpg', 'axnqbt.jpg'
+        'ga7agq.jpg', 'gdl2m4.jpg', 'g3p5t6.jpg', 'g3qflx.jpg', 'd4rzqt.jpg', 'axnqbt.jpg',
+        'dwhzy7.jpg', 'bpxalf.jpg'  # tambahan baru
     ],
     "Ayam Betutu": [
         'ygu7z3.jpg', 'ypqd48.jpg', 'xfqise.jpg', 'vt92ep.jpg', 'vd7urd.jpg', 'upblhd.jpg', 'udzjtt.jpg',
@@ -12,37 +13,43 @@ label_dict = {
     "Ayam Goreng": [
         'osjvxg.jpg', 'qujnw0.jpg', 'zhn32w.jpg', 'z07piz.jpg', 'y4lvap.jpg', 'ycdac0.jpg', 'xdb8lm.jpg', 
         'nn6ekv.jpg', 'nquemu.jpg', 'n705xf.jpg', 'mex70x.jpg', 'mkquio.jpg', 'm3trfn.jpg', 'lll7ke.jpg', 
-        'kykkwi.jpg', 'l0nnmu.jpg', 'k0kqf4.jpg', 'kanelc.jpg', 'vw34ya.jpg', 'g1ln16.jpg',
+        'kykkwi.jpg', 'l0nnmu.jpg', 'k0kqf4.jpg', 'kanelc.jpg', 'vw34ya.jpg', 'g1ln16.jpg'
     ],
     "Ayam Pop": [
         'oou5up.jpg', 'odz928.jpg', 'nustdy.jpg', 'o2l1hx.jpg', 'mxav50.jpg', 'n31rta.jpg', 'n228a6.jpg', 
         'zsmnpe.jpg', 'zfwsk9.jpg', 'z1v1f8.jpg', 'z4amw4.jpg', 'z5o4n2.jpg', 'z34eni.jpg', 'z60vs6.jpg', 
-        'y7rkto.jpg', 'y7iwu2.jpg', 'xctf80.jpg', 'w3wsz2.jpg', 'morqij.jpg', 'yn0078.jpg'
+        'y7rkto.jpg', 'y7iwu2.jpg', 'xctf80.jpg', 'w3wsz2.jpg', 'morqij.jpg', 'yn0078.jpg',
+        '6likum.jpg', '2ip0aa.jpg'  # tambahan baru
     ],
     "Bakso": [
         'muqjiv.jpg', 'm0q8mr.jpg', 'lklwlp.jpg', 'ln8hpq.jpg', 'lpbmcl.jpg', 'lor0kr.jpg', 'lcqd98.jpg', 
         'zw2rjh.jpg', 'znxlpx.jpg', 'z9nnkd.jpg', 'zax7cb.jpg', 'zdkruq.jpg', 'ysz8wk.jpg', 'yxwli2.jpg', 
-        'xslrlc.jpg', 'xbh5l4.jpg', 'xgeyg6.jpg', 'wk46zj.jpg', 'kz1ptl.jpg','z6eglr.jpg'
+        'xslrlc.jpg', 'xbh5l4.jpg', 'xgeyg6.jpg', 'wk46zj.jpg', 'kz1ptl.jpg','z6eglr.jpg',
+        '1z3b7c.jpg', 'xdzyxr.jpg'  # tambahan baru
     ],
     "Coto Makassar": [
         'l2v5k1.jpg', 'jvbwe8.jpg', 'j5jd2u.jpg', 'jb3t6o.jpg', 'i6kcfk.jpg', 'h71uty.jpg', 'fonqs2.jpg', 
         'zte3xr.jpg', 'ztqgfl.jpg', 'y9evk4.jpg', 'xavqmm.jpg', 'wzggze.jpg', 'x57avf.jpg', 'vgocao.jpg',
-        'vnwxs5.jpg', 'uxr2zd.jpg', 'uor0sm.jpg', 'u1os5m.jpg', 'ex3qjn.jpg', 'vlcfwi.jpg'
+        'vnwxs5.jpg', 'uxr2zd.jpg', 'uor0sm.jpg', 'u1os5m.jpg', 'ex3qjn.jpg', 'vlcfwi.jpg',
+        'wen60k.jpg', '87f844.jpg'  # tambahan baru
     ],
     "Gado Gado": [
         '9skpi5.jpg', '9ro9eh.jpg', '9x8owp.jpg', '91lz87.jpg', '8qchfx.jpg', '8s6kf8.jpg', '8exb87.jpg',
         'zh6js1.jpg', 'ysr9ym.jpg', 'z0es0r.jpg', 'ykk3lw.jpg', 'ycu9wz.jpg', 'xk9k4m.jpg', 'wwxbjf.jpg', 
-        'wfz75c.jpg', 'vhgf0s.jpg', 'u3fvs6.jpg', 'tsl7bd.jpg', '84mj9v.jpg', 'we5nwp.jpg'
+        'wfz75c.jpg', 'vhgf0s.jpg', 'u3fvs6.jpg', 'tsl7bd.jpg', '84mj9v.jpg', 'we5nwp.jpg',
+        's6qgjc.jpg', 'jcror9.jpg', '5uk2ca.jpg', '5cbn0o.jpg', '8dbcuj.jpg'  # tambahan baru
     ],
     "Gudeg": [
         'wgnpok.jpg', 'vqrbrp.jpg', 'vs2xna.jpg', 'vttgn3.jpg', 'vwrsky.jpg', 'w6yn02.jpg', 'uxl04w.jpg', 
         'su8rbx.jpg', 'sx8sh7.jpg', 's6v0d4.jpg', 'qyg63n.jpg', 'posxbw.jpg', 'pcfzp4.jpg', 'pci4oj.jpg', 
-        'mv4g7m.jpg', 'jls4p0.jpg', 'jdrt61.jpg', 'jeqyd8.jpg', 'v5hi5e.jpg', 'oyr468.jpg'
+        'mv4g7m.jpg', 'jls4p0.jpg', 'jdrt61.jpg', 'jeqyd8.jpg', 'v5hi5e.jpg', 'oyr468.jpg',
+        'nene62.jpg'  # tambahan baru
     ],
     "Nasi Goreng": [
         'x7lih8.jpg', 'uj8i6c.jpg', 'xdeqr5.jpg', 'zyvkir.jpg', 'yp351h.jpg', 'z81cp5.jpg', 'zinra3.jpg',
         '19icyo.jpg', '11z4yt.jpg', '0aiagz.jpg', '5bsfov.jpg', '2e7igv.jpg', '1v9bz6.jpg', '1dtrgj.jpg',
-        '0i9h44.jpg', '0i1gxc.jpg', '0cczsl.jpg', '0bh6sh.jpg', '00sv9l.jpg', '4427yo.jpg'
+        '0i9h44.jpg', '0i1gxc.jpg', '0cczsl.jpg', '0bh6sh.jpg', '00sv9l.jpg', '4427yo.jpg',
+        '885f7b.jpg', 'se72k1.jpg', '48ms9j.jpg'  # tambahan baru
     ],
     "Pempek": [
         'ye59oz.jpg', 'ypg62x.jpg', 'zprbi0.jpg', 'xg9068.jpg', 'ycxjkl.jpg', 'wq8hfg.jpg', 'qe20dk.jpg',
@@ -57,7 +64,8 @@ label_dict = {
     "Rendang": [
         'r1dwjr.jpg', 'n7nm4v.jpg', 'oyiy5b.jpg', 'tqw5kn.jpg', 'tklbvc.jpg', 'q7izrp.jpg', 'treiw0.jpg',
         '5596vl.jpg', '9hlfxr.jpg', '8harg6.jpg', '7sdrcv.jpg', '230e1n.jpg', '29u1i4.jpg', '05rl1p.jpg',
-        '2g9yhj.jpg', '1z746f.jpg', '1ws386.jpg', '5koy6m.jpg', '2jynpg.jpg', '3c68du.jpg'
+        '2g9yhj.jpg', '1z746f.jpg', '1ws386.jpg', '5koy6m.jpg', '2jynpg.jpg', '3c68du.jpg',
+        'gf8act.jpg', 'l5yf76.jpg', 'baqkam.jpg'  # tambahan baru
     ],
     "Sate Madura": [
         't2rvsa.jpg', 'souf92.jpg', 'rxjuqb.jpg', 'sj0bdq.jpg', 'sfdgq3.jpg', 'sabp9v.jpg', 'ru4er4.jpg',
@@ -72,6 +80,7 @@ label_dict = {
     "Soto": [
         'dlt2qa.jpg', 'a44jun.jpg', 'ajy0b6.jpg', 'g52zkp.jpg', 'ga5p62.jpg', 'h0plhm.jpg', 'hi7ohy.jpg',
         '6tu7qi.jpg', 'bxm43j.jpg', 'j6jy3n.jpg', 'py8rv7.jpg', '6ln0r0.jpg', 'c8j0xd.jpg', 'gltm2j.jpg',
-        'ggve3n.jpg', 'ke5qc7.jpg', 'k68gfn.jpg', 'iqonfz.jpg', 'i1pnbe.jpg'
+        'ggve3n.jpg', 'ke5qc7.jpg', 'k68gfn.jpg', 'iqonfz.jpg', 'i1pnbe.jpg',
+        'e4dotv.jpg', 'lpuwvq.jpg', 'mhxypc.jpg'  # tambahan baru
     ]
 }
