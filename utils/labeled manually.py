f28fcf4d3950fd6933edca28a56d12a16e2ff78c
kalmanachmad <150771003+kalmanachmad@users.noreply.github.com>
2025-11-01 19:50:29 +0700

    labeled manually tambahan.py

--- utils/labeled manually.py
+++ utils/labeled manually.py
@@ -1,86 +1,121 @@
 label_dict = {
     "Ayam Bakar": [
-        'zt93li.jpg', 'ztaz8z.jpg', 'zd4bwe.jpg', 'zgp5q9.jpg', 'ytcwjz.jpg', 'ywtpui.jpg', 'ywx5mg.jpg',
-        'z0zm3z.jpg', 'irh22j.jpg', 'ifc83g.jpg', 'hzgnpg.jpg', 'o7ulw5.jpg', 'hop7y8.jpg', 'gr31o3.jpg',
-        'ga7agq.jpg', 'gdl2m4.jpg', 'g3p5t6.jpg', 'g3qflx.jpg', 'd4rzqt.jpg', 'axnqbt.jpg',
-        'dwhzy7.jpg', 'bpxalf.jpg'  # tambahan baru
+        "ztqy3o.jpg", "jr6olg.jpg", "w685il.jpg", "c1qcw5.jpg", "nz2ewj.jpg", "nt3ucf.jpg", "jfrvoc.jpg",
+        "o58819.jpg", "8fl8pm.jpg", "xvw9sg.jpg", "t6vtgn.jpg", "v5hzuc.jpg", "t6dgrp.jpg", "7rz451.jpg",
+        "5pl9tb.jpg", "ehjp2j.jpg", "44locs.jpg", "l0xz3s.jpg", "zt93li.jpg", "ztaz8z.jpg", "zd4bwe.jpg",
+        "zgp5q9.jpg", "ytcwjz.jpg", "ywtpui.jpg", "ywx5mg.jpg", "z0zm3z.jpg", "irh22j.jpg", "ifc83g.jpg",
+        "hzgnpg.jpg", "o7ulw5.jpg", "hop7y8.jpg", "gr31o3.jpg", "ga7agq.jpg", "gdl2m4.jpg", "g3p5t6.jpg",
+        "g3qflx.jpg", "d4rzqt.jpg", "axnqbt.jpg", "dwhzy7.jpg", "bpxalf.jpg"
     ],
     "Ayam Betutu": [
-        'ygu7z3.jpg', 'ypqd48.jpg', 'xfqise.jpg', 'vt92ep.jpg', 'vd7urd.jpg', 'upblhd.jpg', 'udzjtt.jpg',
-        'o30wxz.jpg', 'ljwlzv.jpg', 'zcizei.jpg', 'yzqjn8.jpg', 'oetw8i.jpg', 'nw6y1a.jpg', 'kwuynu.jpg',
-        'mtbsd6.jpg', 'mcj9m3.jpg', 'mbij5m.jpg', 'ly0p05.jpg', 'tv0eau.jpg', 'n3jkct.jpg'
+        "qmn49q.jpg", "lilwde.jpg", "f4cpaz.jpg", "8w0uhq.jpg", "tg7p1x.jpg", "ccedp2.jpg", "rdcds2.jpg",
+        "qvu4k2.jpg", "cs1qe9.jpg", "rkjsih.jpg", "oolapd.jpg", "wn09k1.jpg", "20ffzb.jpg", "nw3eym.jpg",
+        "543fwz.jpg", "i4yyry.jpg", "2ebndh.jpg", "bn93er.jpg", "oywcn1.jpg", "behbeo.jpg", "ni5cjf.jpg",
+        "1xkf9s.jpg", "s58hn5.jpg", "94hgwu.jpg", "lhojpe.jpg", "2ftkwk.jpg", "ygu7z3.jpg", "ypqd48.jpg",
+        "xfqise.jpg", "vt92ep.jpg", "vd7urd.jpg", "upblhd.jpg", "udzjtt.jpg", "o30wxz.jpg", "ljwlzv.jpg",
+        "zcizei.jpg", "yzqjn8.jpg", "oetw8i.jpg", "nw6y1a.jpg", "kwuynu.jpg", "mtbsd6.jpg", "mcj9m3.jpg",
+        "mbij5m.jpg", "ly0p05.jpg", "tv0eau.jpg", "n3jkct.jpg"
     ],
     "Ayam Goreng": [
-        'osjvxg.jpg', 'qujnw0.jpg', 'zhn32w.jpg', 'z07piz.jpg', 'y4lvap.jpg', 'ycdac0.jpg', 'xdb8lm.jpg', 
-        'nn6ekv.jpg', 'nquemu.jpg', 'n705xf.jpg', 'mex70x.jpg', 'mkquio.jpg', 'm3trfn.jpg', 'lll7ke.jpg', 
-        'kykkwi.jpg', 'l0nnmu.jpg', 'k0kqf4.jpg', 'kanelc.jpg', 'vw34ya.jpg', 'g1ln16.jpg'
+        "wr88dd.jpg", "0u30lb.jpg", "iyofo4.jpg", "5fp27u.jpg", "q2ynmb.jpg", "05jpj7.jpg", "mn734i.jpg",
+        "b7rfli.jpg", "9bmxhr.jpg", "g9qsqo.jpg", "89kb7n.jpg", "x3w0ds.jpg", "98c8g2.jpg", "fytpjo.jpg",
+        "anuf5o.jpg", "b41cwd.jpg", "spr1fa.jpg", "3pt42l.jpg", "4nso9p.jpg", "rqkau3.jpg", "7c6dh2.jpg",
+        "gz8kgf.jpg", "527xvc.jpg", "k90kew.jpg", "0j943c.jpg", "m7q163.jpg", "bnlrt9.jpg", "h9rib9.jpg",
+        "3o1nww.jpg", "k99slb.jpg", "osjvxg.jpg", "qujnw0.jpg", "zhn32w.jpg", "z07piz.jpg", "y4lvap.jpg",
+        "ycdac0.jpg", "xdb8lm.jpg", "nn6ekv.jpg", "nquemu.jpg", "n705xf.jpg", "mex70x.jpg", "mkquio.jpg",
+        "m3trfn.jpg", "lll7ke.jpg", "kykkwi.jpg", "l0nnmu.jpg", "k0kqf4.jpg", "kanelc.jpg", "vw34ya.jpg", "g1ln16.jpg"
     ],
     "Ayam Pop": [
-        'oou5up.jpg', 'odz928.jpg', 'nustdy.jpg', 'o2l1hx.jpg', 'mxav50.jpg', 'n31rta.jpg', 'n228a6.jpg', 
-        'zsmnpe.jpg', 'zfwsk9.jpg', 'z1v1f8.jpg', 'z4amw4.jpg', 'z5o4n2.jpg', 'z34eni.jpg', 'z60vs6.jpg', 
-        'y7rkto.jpg', 'y7iwu2.jpg', 'xctf80.jpg', 'w3wsz2.jpg', 'morqij.jpg', 'yn0078.jpg',
-        '6likum.jpg', '2ip0aa.jpg'  # tambahan baru
+        "467gh6.jpg", "9gus7u.jpg", "6oxra8.jpg", "ptiblk.jpg", "9klzel.jpg", "6iskow.jpg", "5f4n6f.jpg",
+        "9jpf9z.jpg", "knoqa8.jpg", "5q1nxy.jpg", "u07hgs.jpg", "du7zfb.jpg", "iox7yj.jpg", "mcfffm.jpg",
+        "w6y0db.jpg", "oou5up.jpg", "odz928.jpg", "nustdy.jpg", "o2l1hx.jpg", "mxav50.jpg", "n31rta.jpg",
+        "n228a6.jpg", "zsmnpe.jpg", "zfwsk9.jpg", "z1v1f8.jpg", "z4amw4.jpg", "z5o4n2.jpg", "z34eni.jpg",
+        "z60vs6.jpg", "y7rkto.jpg", "y7iwu2.jpg", "xctf80.jpg", "w3wsz2.jpg", "morqij.jpg", "yn0078.jpg",
+        "6likum.jpg", "2ip0aa.jpg"
     ],
     "Bakso": [
-        'muqjiv.jpg', 'm0q8mr.jpg', 'lklwlp.jpg', 'ln8hpq.jpg', 'lpbmcl.jpg', 'lor0kr.jpg', 'lcqd98.jpg', 
-        'zw2rjh.jpg', 'znxlpx.jpg', 'z9nnkd.jpg', 'zax7cb.jpg', 'zdkruq.jpg', 'ysz8wk.jpg', 'yxwli2.jpg', 
-        'xslrlc.jpg', 'xbh5l4.jpg', 'xgeyg6.jpg', 'wk46zj.jpg', 'kz1ptl.jpg','z6eglr.jpg',
-        '1z3b7c.jpg', 'xdzyxr.jpg'  # tambahan baru
+        "7eotob.jpg", "nuxn9u.jpg", "6d2buk.jpg", "fj3mqv.jpg", "m84dg8.jpg", "dvlqtx.jpg", "bgewol.jpg",
+        "5dafar.jpg", "nymre8.jpg", "zoyrp3.jpg", "8gjh9g.jpg", "hh40r0.jpg", "r1qq4h.jpg", "bxnhy2.jpg",
+        "0a4zbm.jpg", "nvfmij.jpg", "3d736x.jpg", "k7bdxq.jpg", "001g4d.jpg", "amcsrq.jpg", "emgc3f.jpg",
+        "qfg9gs.jpg", "zqoqr1.jpg", "wdeka9.jpg", "31ngz4.jpg", "muqjiv.jpg", "m0q8mr.jpg", "lklwlp.jpg",
+        "ln8hpq.jpg", "lpbmcl.jpg", "lor0kr.jpg", "lcqd98.jpg", "zw2rjh.jpg", "znxlpx.jpg", "z9nnkd.jpg",
+        "zax7cb.jpg", "zdkruq.jpg", "ysz8wk.jpg", "yxwli2.jpg", "xslrlc.jpg", "xbh5l4.jpg", "xgeyg6.jpg",
+        "wk46zj.jpg", "kz1ptl.jpg", "z6eglr.jpg", "1z3b7c.jpg", "xdzyxr.jpg"
     ],
     "Coto Makassar": [
-        'l2v5k1.jpg', 'jvbwe8.jpg', 'j5jd2u.jpg', 'jb3t6o.jpg', 'i6kcfk.jpg', 'h71uty.jpg', 'fonqs2.jpg', 
-        'zte3xr.jpg', 'ztqgfl.jpg', 'y9evk4.jpg', 'xavqmm.jpg', 'wzggze.jpg', 'x57avf.jpg', 'vgocao.jpg',
-        'vnwxs5.jpg', 'uxr2zd.jpg', 'uor0sm.jpg', 'u1os5m.jpg', 'ex3qjn.jpg', 'vlcfwi.jpg',
-        'wen60k.jpg', '87f844.jpg'  # tambahan baru
+        "a1ao2b.jpg", "9xw8fp.jpg", "n8k875.jpg", "pxivt4.jpg", "3811or.jpg", "htb2kj.jpg", "rzlej0.jpg",
+        "7mxd2g.jpg", "lot0lh.jpg", "eby12m.jpg", "kz5axf.jpg", "t1ozek.jpg", "f8yhlx.jpg", "z6e45k.jpg",
+        "l2v5k1.jpg", "jvbwe8.jpg", "j5jd2u.jpg", "jb3t6o.jpg", "i6kcfk.jpg", "h71uty.jpg", "fonqs2.jpg",
+        "zte3xr.jpg", "ztqgfl.jpg", "y9evk4.jpg", "xavqmm.jpg", "wzggze.jpg", "x57avf.jpg", "vgocao.jpg",
+        "vnwxs5.jpg", "uxr2zd.jpg", "uor0sm.jpg", "u1os5m.jpg", "ex3qjn.jpg", "vlcfwi.jpg", "wen60k.jpg", "87f844.jpg"
     ],
     "Gado Gado": [
-        '9skpi5.jpg', '9ro9eh.jpg', '9x8owp.jpg', '91lz87.jpg', '8qchfx.jpg', '8s6kf8.jpg', '8exb87.jpg',
-        'zh6js1.jpg', 'ysr9ym.jpg', 'z0es0r.jpg', 'ykk3lw.jpg', 'ycu9wz.jpg', 'xk9k4m.jpg', 'wwxbjf.jpg', 
-        'wfz75c.jpg', 'vhgf0s.jpg', 'u3fvs6.jpg', 'tsl7bd.jpg', '84mj9v.jpg', 'we5nwp.jpg',
-        's6qgjc.jpg', 'jcror9.jpg', '5uk2ca.jpg', '5cbn0o.jpg', '8dbcuj.jpg'  # tambahan baru
+        "qcd0pi.jpg", "pwis1w.jpg", "vp5zwr.jpg", "6vhzqv.jpg", "0je5k7.jpg", "xbs407.jpg", "nd3gds.jpg",
+        "q3ik9j.jpg", "ml2a57.jpg", "14tnr5.jpg", "7zfou9.jpg", "pdrrsd.jpg", "lfz1nn.jpg", "4bppkr.jpg",
+        "yo9ha2.jpg", "4yc5rk.jpg", "qs69ee.jpg", "vf9l0x.jpg", "8nexiw.jpg", "ld1dk4.jpg", "kbiakn.jpg",
+        "9skpi5.jpg", "9ro9eh.jpg", "9x8owp.jpg", "91lz87.jpg", "8qchfx.jpg", "8s6kf8.jpg", "8exb87.jpg",
+        "zh6js1.jpg", "ysr9ym.jpg", "z0es0r.jpg", "ykk3lw.jpg", "ycu9wz.jpg", "xk9k4m.jpg", "wwxbjf.jpg",
+        "wfz75c.jpg", "vhgf0s.jpg", "u3fvs6.jpg", "tsl7bd.jpg", "84mj9v.jpg", "we5nwp.jpg", "s6qgjc.jpg",
+        "jcror9.jpg", "5uk2ca.jpg", "5cbn0o.jpg", "8dbcuj.jpg"
     ],
     "Gudeg": [
-        'wgnpok.jpg', 'vqrbrp.jpg', 'vs2xna.jpg', 'vttgn3.jpg', 'vwrsky.jpg', 'w6yn02.jpg', 'uxl04w.jpg', 
-        'su8rbx.jpg', 'sx8sh7.jpg', 's6v0d4.jpg', 'qyg63n.jpg', 'posxbw.jpg', 'pcfzp4.jpg', 'pci4oj.jpg', 
-        'mv4g7m.jpg', 'jls4p0.jpg', 'jdrt61.jpg', 'jeqyd8.jpg', 'v5hi5e.jpg', 'oyr468.jpg',
-        'nene62.jpg'  # tambahan baru
+        "3zkmn8.jpg", "fdqm68.jpg", "yhpp0w.jpg", "vts83l.jpg", "oyl7r3.jpg", "mer6lb.jpg", "ja4htw.jpg",
+        "8q3k3k.jpg", "jk32hr.jpg", "pdk97y.jpg", "pomifm.jpg", "pboqzl.jpg", "hv53kn.jpg", "ggnwrs.jpg",
+        "vyu4s1.jpg", "7xrp8g.jpg", "qpwjg4.jpg", "30f84k.jpg", "y8vfvk.jpg", "wgnpok.jpg", "vqrbrp.jpg",
+        "vs2xna.jpg", "vttgn3.jpg", "vwrsky.jpg", "w6yn02.jpg", "uxl04w.jpg", "su8rbx.jpg", "sx8sh7.jpg",
+        "s6v0d4.jpg", "qyg63n.jpg", "posxbw.jpg", "pcfzp4.jpg", "pci4oj.jpg", "mv4g7m.jpg", "jls4p0.jpg",
+        "jdrt61.jpg", "jeqyd8.jpg", "v5hi5e.jpg", "oyr468.jpg", "nene62.jpg"
     ],
     "Nasi Goreng": [
-        'x7lih8.jpg', 'uj8i6c.jpg', 'xdeqr5.jpg', 'zyvkir.jpg', 'yp351h.jpg', 'z81cp5.jpg', 'zinra3.jpg',
-        '19icyo.jpg', '11z4yt.jpg', '0aiagz.jpg', '5bsfov.jpg', '2e7igv.jpg', '1v9bz6.jpg', '1dtrgj.jpg',
-        '0i9h44.jpg', '0i1gxc.jpg', '0cczsl.jpg', '0bh6sh.jpg', '00sv9l.jpg', '4427yo.jpg',
-        '885f7b.jpg', 'se72k1.jpg', '48ms9j.jpg'  # tambahan baru
+        "ywrdwy.jpg", "fg1ax9.jpg", "4ymxbv.jpg", "d8nxuv.jpg", "7swrro.jpg", "jo34e4.jpg", "ie80bn.jpg",
+        "wsbqjw.jpg", "6s3ikx.jpg", "nvk29c.jpg", "9cgklr.jpg", "woz1wb.jpg", "dv0v9o.jpg", "hhaydl.jpg",
+        "2gkvxq.jpg", "rtj060.jpg", "zorsx3.jpg", "zwofq4.jpg", "47tdl8.jpg", "crxaub.jpg", "ts97by.jpg",
+        "of2v92.jpg", "robzaw.jpg", "n15gzn.jpg", "k57ok4.jpg", "x7lih8.jpg", "uj8i6c.jpg", "xdeqr5.jpg",
+        "zyvkir.jpg", "yp351h.jpg", "z81cp5.jpg", "zinra3.jpg", "19icyo.jpg", "11z4yt.jpg", "0aiagz.jpg",
+        "5bsfov.jpg", "2e7igv.jpg", "1v9bz6.jpg", "1dtrgj.jpg", "0i9h44.jpg", "0i1gxc.jpg", "0cczsl.jpg",
+        "0bh6sh.jpg", "00sv9l.jpg", "4427yo.jpg", "885f7b.jpg", "se72k1.jpg", "48ms9j.jpg"
     ],
     "Pempek": [
-        'ye59oz.jpg', 'ypg62x.jpg', 'zprbi0.jpg', 'xg9068.jpg', 'ycxjkl.jpg', 'wq8hfg.jpg', 'qe20dk.jpg',
-        '915nvv.jpg', '9whi9y.jpg', '8x53sf.jpg', '7ufolm.jpg', '5bogwu.jpg', '4mc4wr.jpg', '4kfq4b.jpg',
-        '3yrmom.jpg', '99856s.jpg', '9w4enl.jpg', '9resvi.jpg', '7mrby1.jpg', '7c0l6c.jpg'
+        "5bjuxb.jpg", "jpoh5g.jpg", "l76w1a.jpg", "r36pzw.jpg", "wqjqj6.jpg", "hhhgmp.jpg", "a7ovsa.jpg",
+        "b13dqn.jpg", "p0vlh8.jpg", "9kvvqn.jpg", "jgfc52.jpg", "nu7wmm.jpg", "thrv5n.jpg", "eef3pb.jpg",
+        "bq2r49.jpg", "ye59oz.jpg", "ypg62x.jpg", "zprbi0.jpg", "xg9068.jpg", "ycxjkl.jpg", "wq8hfg.jpg",
+        "qe20dk.jpg", "915nvv.jpg", "9whi9y.jpg", "8x53sf.jpg", "7ufolm.jpg", "5bogwu.jpg", "4mc4wr.jpg",
+        "4kfq4b.jpg", "3yrmom.jpg", "99856s.jpg", "9w4enl.jpg", "9resvi.jpg", "7mrby1.jpg", "7c0l6c.jpg"
     ],
     "Rawon": [
-        'zp5smb.jpg', 'vq96ba.jpg', 'y13sh9.jpg', 'xaibrr.jpg', 'xf8pf8.jpg', 'xe09c4.jpg', 'ygd37o.jpg',
-        '3w3poi.jpg', '68mly1.jpg', '8zaug5.jpg', 'a6ovv0.jpg', '06ni3h.jpg', '6ualqk.jpg', 'bx0fdi.jpg',
-        'bpd5rj.jpg', 'b7z0dj.jpg', 'hkpad9.jpg', 'epek9v.jpg', 'emdrz5.jpg', 'm1k2x8.jpg'
+        "tl3xrw.jpg", "mg0gmg.jpg", "o343bz.jpg", "qzamp6.jpg", "3mj09q.jpg", "li3p9g.jpg", "w8ciua.jpg",
+        "fej5c6.jpg", "53ikqb.jpg", "5i7dr4.jpg", "6jl65.jpg", "hr0mp5.jpg", "gc5a17.jpg", "onctf9.jpg",
+        "m23fdz.jpg", "j736gc.jpg", "gjrko1.jpg", "glfwz1.jpg", "9d0hyy.jpg", "zp5smb.jpg", "vq96ba.jpg",
+        "y13sh9.jpg", "xaibrr.jpg", "xf8pf8.jpg", "xe09c4.jpg", "ygd37o.jpg", "3w3poi.jpg", "68mly1.jpg",
+        "8zaug5.jpg", "a6ovv0.jpg", "06ni3h.jpg", "6ualqk.jpg", "bx0fdi.jpg", "bpd5rj.jpg", "b7z0dj.jpg",
+        "hkpad9.jpg", "epek9v.jpg", "emdrz5.jpg", "m1k2x8.jpg"
     ],
     "Rendang": [
-        'r1dwjr.jpg', 'n7nm4v.jpg', 'oyiy5b.jpg', 'tqw5kn.jpg', 'tklbvc.jpg', 'q7izrp.jpg', 'treiw0.jpg',
-        '5596vl.jpg', '9hlfxr.jpg', '8harg6.jpg', '7sdrcv.jpg', '230e1n.jpg', '29u1i4.jpg', '05rl1p.jpg',
-        '2g9yhj.jpg', '1z746f.jpg', '1ws386.jpg', '5koy6m.jpg', '2jynpg.jpg', '3c68du.jpg',
-        'gf8act.jpg', 'l5yf76.jpg', 'baqkam.jpg'  # tambahan baru
+        "tshgb1.jpg", "oa6s97.jpg", "t3xubf.jpg", "bcyieq.jpg", "79034h.jpg", "a4rimo.jpg", "rtpav4.jpg",
+        "ov7pvs.jpg", "i8ykv9.jpg", "2gv3xl.jpg", "1kk93w.jpg", "zw4a0b.jpg", "rw9m0c.jpg", "t6474e.jpg",
+        "g01hd6.jpg", "uw8jaq.jpg", "ox0yiz.jpg", "x2ngtm.jpg", "j3cj82.jpg", "0n53jf.jpg", "r1dwjr.jpg",
+        "n7nm4v.jpg", "oyiy5b.jpg", "tqw5kn.jpg", "tklbvc.jpg", "q7izrp.jpg", "treiw0.jpg", "5596vl.jpg",
+        "9hlfxr.jpg", "8harg6.jpg", "7sdrcv.jpg", "230e1n.jpg", "29u1i4.jpg", "05rl1p.jpg", "2g9yhj.jpg",
+        "1z746f.jpg", "1ws386.jpg", "5koy6m.jpg", "2jynpg.jpg", "3c68du.jpg", "gf8act.jpg", "l5yf76.jpg", "baqkam.jpg"
     ],
     "Sate Madura": [
-        't2rvsa.jpg', 'souf92.jpg', 'rxjuqb.jpg', 'sj0bdq.jpg', 'sfdgq3.jpg', 'sabp9v.jpg', 'ru4er4.jpg',
-        '073u7f.jpg', '17zxr8.jpg', 'xkgods.jpg', '5ytesw.jpg', '274329.jpg', '044hgc.jpg', '134ovu.jpg',
-        '27h8hs.jpg', '40ma7s.jpg', '38ccq9.jpg', '30v05m.jpg', '5u89p9.jpg', '4voy9c.jpg'
+        "8setq1.jpg", "1tixag.jpg", "zggq0o.jpg", "u2aviq.jpg", "oza4lj.jpg", "wiytaw.jpg", "7ku8dd.jpg", "20wnhk.jpg",
+        "t2rvsa.jpg", "souf92.jpg", "rxjuqb.jpg", "sj0bdq.jpg", "sfdgq3.jpg", "sabp9v.jpg", "ru4er4.jpg", "073u7f.jpg",
+        "17zxr8.jpg", "xkgods.jpg", "5ytesw.jpg", "274329.jpg", "044hgc.jpg", "134ovu.jpg", "27h8hs.jpg", "40ma7s.jpg",
+        "38ccq9.jpg", "30v05m.jpg", "5u89p9.jpg", "4voy9c.jpg"
     ],
     "Sate Padang": [
-        'io72zl.jpg', 'jjpnkj.jpg', 'jmgp9d.jpg', 'i2v6f7.jpg', 'ibb7np.jpg', 'oaaxob.jpg', 'pu3gos.jpg',
-        '0us23t.jpg', '0ulszd.jpg', '0pnjze.jpg', '9xwtq3.jpg', '19tv5c.jpg', '400eom.jpg', '5bchh9.jpg',
-        '846gqq.jpg', '80evge.jpg', '73wypf.jpg', '8xwlpp.jpg', '8he9j8.jpg', '7pjfqb.jpg'
+        "m95dai.jpg", "mdktzf.jpg", "d2d8x7.jpg", "7kbp43.jpg", "b0bo3t.jpg", "lkadmu.jpg", "qb6cjs.jpg",
+        "nywhe8.jpg", "29vvh7.jpg", "x6wbt7.jpg", "io72zl.jpg", "jjpnkj.jpg", "jmgp9d.jpg", "i2v6f7.jpg",
+        "ibb7np.jpg", "oaaxob.jpg", "pu3gos.jpg", "0us23t.jpg", "0ulszd.jpg", "0pnjze.jpg", "9xwtq3.jpg",
+        "19tv5c.jpg", "400eom.jpg", "5bchh9.jpg", "846gqq.jpg", "80evge.jpg", "73wypf.jpg", "8xwlpp.jpg",
+        "8he9j8.jpg", "7pjfqb.jpg"
     ],
     "Soto": [
-        'dlt2qa.jpg', 'a44jun.jpg', 'ajy0b6.jpg', 'g52zkp.jpg', 'ga5p62.jpg', 'h0plhm.jpg', 'hi7ohy.jpg',
-        '6tu7qi.jpg', 'bxm43j.jpg', 'j6jy3n.jpg', 'py8rv7.jpg', '6ln0r0.jpg', 'c8j0xd.jpg', 'gltm2j.jpg',
-        'ggve3n.jpg', 'ke5qc7.jpg', 'k68gfn.jpg', 'iqonfz.jpg', 'i1pnbe.jpg',
-        'e4dotv.jpg', 'lpuwvq.jpg', 'mhxypc.jpg'  # tambahan baru
+        "qfnqdh.jpg", "g4rc8o.jpg", "bnyf4s.jpg", "cb3je6.jpg", "y2wlt3.jpg", "b32amx.jpg", "uz8vpn.jpg",
+        "zar2dz.jpg", "3kx7k0.jpg", "sbo0me.jpg", "gvsvqv.jpg", "nph7w2.jpg", "l89ykz.jpg", "jwg6in.jpg", "15xaty.jpg",
+        "dlt2qa.jpg", "a44jun.jpg", "ajy0b6.jpg", "g52zkp.jpg", "ga5p62.jpg", "h0plhm.jpg", "hi7ohy.jpg", "6tu7qi.jpg",
+        "bxm43j.jpg", "j6jy3n.jpg", "py8rv7.jpg", "6ln0r0.jpg", "c8j0xd.jpg", "gltm2j.jpg", "ggve3n.jpg", "ke5qc7.jpg",
+        "k68gfn.jpg", "iqonfz.jpg", "i1pnbe.jpg", "e4dotv.jpg", "lpuwvq.jpg", "mhxypc.jpg"
     ]
 }
