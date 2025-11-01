3b6ba95b31aba216d415989e2508b9e34a3cb34a
kalmanachmad <150771003+kalmanachmad@users.noreply.github.com>
2025-11-01 23:31:28 +0700

    Update labeled manually.py

--- utils/labeled manually.py
+++ utils/labeled manually.py
@@ -5,7 +5,9 @@ label_dict = {
         "5pl9tb.jpg", "ehjp2j.jpg", "44locs.jpg", "l0xz3s.jpg", "zt93li.jpg", "ztaz8z.jpg", "zd4bwe.jpg",
         "zgp5q9.jpg", "ytcwjz.jpg", "ywtpui.jpg", "ywx5mg.jpg", "z0zm3z.jpg", "irh22j.jpg", "ifc83g.jpg",
         "hzgnpg.jpg", "o7ulw5.jpg", "hop7y8.jpg", "gr31o3.jpg", "ga7agq.jpg", "gdl2m4.jpg", "g3p5t6.jpg",
-        "g3qflx.jpg", "d4rzqt.jpg", "axnqbt.jpg", "dwhzy7.jpg", "bpxalf.jpg"
+        "g3qflx.jpg", "d4rzqt.jpg", "axnqbt.jpg", "dwhzy7.jpg", "bpxalf.jpg",
+        "s189lz.jpg", "04gh8d.jpg", "n1ataq.jpg", "0g13wo.jpg", "cbafe5.jpg", "dchpo0.jpg", "txau4r.jpg",
+        "vjboz1.jpg", "5oolr6.jpg", "yzfeno.jpg", "6dfitv.jpg", "2cwt10.jpg", "3v2l36.jpg"
     ],
     "Ayam Betutu": [
         "qmn49q.jpg", "lilwde.jpg", "f4cpaz.jpg", "8w0uhq.jpg", "tg7p1x.jpg", "ccedp2.jpg", "rdcds2.jpg",
@@ -14,7 +16,9 @@ label_dict = {
         "1xkf9s.jpg", "s58hn5.jpg", "94hgwu.jpg", "lhojpe.jpg", "2ftkwk.jpg", "ygu7z3.jpg", "ypqd48.jpg",
         "xfqise.jpg", "vt92ep.jpg", "vd7urd.jpg", "upblhd.jpg", "udzjtt.jpg", "o30wxz.jpg", "ljwlzv.jpg",
         "zcizei.jpg", "yzqjn8.jpg", "oetw8i.jpg", "nw6y1a.jpg", "kwuynu.jpg", "mtbsd6.jpg", "mcj9m3.jpg",
-        "mbij5m.jpg", "ly0p05.jpg", "tv0eau.jpg", "n3jkct.jpg"
+        "mbij5m.jpg", "ly0p05.jpg", "tv0eau.jpg", "n3jkct.jpg",
+        "q1o13g.jpg", "sk07ue.jpg", "hmyth1.jpg", "qp5kic.jpg", "v3w4us.jpg", "ezws7u.jpg", "kgyr32.jpg",
+        "jbh7vp.jpg", "divke7.jpg"
     ],
     "Ayam Goreng": [
         "wr88dd.jpg", "0u30lb.jpg", "iyofo4.jpg", "5fp27u.jpg", "q2ynmb.jpg", "05jpj7.jpg", "mn734i.jpg",
@@ -23,7 +27,9 @@ label_dict = {
         "gz8kgf.jpg", "527xvc.jpg", "k90kew.jpg", "0j943c.jpg", "m7q163.jpg", "bnlrt9.jpg", "h9rib9.jpg",
         "3o1nww.jpg", "k99slb.jpg", "osjvxg.jpg", "qujnw0.jpg", "zhn32w.jpg", "z07piz.jpg", "y4lvap.jpg",
         "ycdac0.jpg", "xdb8lm.jpg", "nn6ekv.jpg", "nquemu.jpg", "n705xf.jpg", "mex70x.jpg", "mkquio.jpg",
-        "m3trfn.jpg", "lll7ke.jpg", "kykkwi.jpg", "l0nnmu.jpg", "k0kqf4.jpg", "kanelc.jpg", "vw34ya.jpg", "g1ln16.jpg"
+        "m3trfn.jpg", "lll7ke.jpg", "kykkwi.jpg", "l0nnmu.jpg", "k0kqf4.jpg", "kanelc.jpg", "vw34ya.jpg", "g1ln16.jpg",
+        "s5oqmo.jpg", "2aqwr1.jpg", "3vdnns.jpg", "ur50d7.jpg", "mjwj4n.jpg", "lzp0tt.jpg", "de0ye7.jpg",
+        "jov47k.jpg", "0wse0g.jpg", "x5v72u.jpg", "u33td1.jpg", "ghrvsx.jpg", "lao6ir.jpg", "fwa9u1.jpg", "gixeid.jpg"
     ],
     "Ayam Pop": [
         "467gh6.jpg", "9gus7u.jpg", "6oxra8.jpg", "ptiblk.jpg", "9klzel.jpg", "6iskow.jpg", "5f4n6f.jpg",
@@ -31,7 +37,8 @@ label_dict = {
         "w6y0db.jpg", "oou5up.jpg", "odz928.jpg", "nustdy.jpg", "o2l1hx.jpg", "mxav50.jpg", "n31rta.jpg",
         "n228a6.jpg", "zsmnpe.jpg", "zfwsk9.jpg", "z1v1f8.jpg", "z4amw4.jpg", "z5o4n2.jpg", "z34eni.jpg",
         "z60vs6.jpg", "y7rkto.jpg", "y7iwu2.jpg", "xctf80.jpg", "w3wsz2.jpg", "morqij.jpg", "yn0078.jpg",
-        "6likum.jpg", "2ip0aa.jpg"
+        "6likum.jpg", "2ip0aa.jpg",
+        "f9zgtg.jpg", "92bewr.jpg", "fbh3gx.jpg", "htf6q9.jpg", "98cp4n.jpg"
     ],
     "Bakso": [
         "7eotob.jpg", "nuxn9u.jpg", "6d2buk.jpg", "fj3mqv.jpg", "m84dg8.jpg", "dvlqtx.jpg", "bgewol.jpg",
@@ -40,14 +47,18 @@ label_dict = {
         "qfg9gs.jpg", "zqoqr1.jpg", "wdeka9.jpg", "31ngz4.jpg", "muqjiv.jpg", "m0q8mr.jpg", "lklwlp.jpg",
         "ln8hpq.jpg", "lpbmcl.jpg", "lor0kr.jpg", "lcqd98.jpg", "zw2rjh.jpg", "znxlpx.jpg", "z9nnkd.jpg",
         "zax7cb.jpg", "zdkruq.jpg", "ysz8wk.jpg", "yxwli2.jpg", "xslrlc.jpg", "xbh5l4.jpg", "xgeyg6.jpg",
-        "wk46zj.jpg", "kz1ptl.jpg", "z6eglr.jpg", "1z3b7c.jpg", "xdzyxr.jpg"
+        "wk46zj.jpg", "kz1ptl.jpg", "z6eglr.jpg", "1z3b7c.jpg", "xdzyxr.jpg",
+        "8c0v4w.jpg", "kcal60.jpg", "7u3w1j.jpg", "pa5702.jpg", "sp0zel.jpg", "eas2lv.jpg", "ii3wdd.jpg",
+        "v6mexc.jpg", "33cq5u.jpg", "ea85nr.jpg"
     ],
     "Coto Makassar": [
         "a1ao2b.jpg", "9xw8fp.jpg", "n8k875.jpg", "pxivt4.jpg", "3811or.jpg", "htb2kj.jpg", "rzlej0.jpg",
         "7mxd2g.jpg", "lot0lh.jpg", "eby12m.jpg", "kz5axf.jpg", "t1ozek.jpg", "f8yhlx.jpg", "z6e45k.jpg",
         "l2v5k1.jpg", "jvbwe8.jpg", "j5jd2u.jpg", "jb3t6o.jpg", "i6kcfk.jpg", "h71uty.jpg", "fonqs2.jpg",
         "zte3xr.jpg", "ztqgfl.jpg", "y9evk4.jpg", "xavqmm.jpg", "wzggze.jpg", "x57avf.jpg", "vgocao.jpg",
-        "vnwxs5.jpg", "uxr2zd.jpg", "uor0sm.jpg", "u1os5m.jpg", "ex3qjn.jpg", "vlcfwi.jpg", "wen60k.jpg", "87f844.jpg"
+        "vnwxs5.jpg", "uxr2zd.jpg", "uor0sm.jpg", "u1os5m.jpg", "ex3qjn.jpg", "vlcfwi.jpg", "wen60k.jpg", "87f844.jpg",
+        "3tkkso.jpg", "6ziwqb.jpg", "7wwlmf.jpg", "fhmzgi.jpg", "b2druv.jpg", "mnolpd.jpg", "6gbtf2.jpg",
+        "dya4ix.jpg", "dpd7ak.jpg", "njvqho.jpg"
     ],
     "Gado Gado": [
         "qcd0pi.jpg", "pwis1w.jpg", "vp5zwr.jpg", "6vhzqv.jpg", "0je5k7.jpg", "xbs407.jpg", "nd3gds.jpg",
@@ -56,7 +67,9 @@ label_dict = {
         "9skpi5.jpg", "9ro9eh.jpg", "9x8owp.jpg", "91lz87.jpg", "8qchfx.jpg", "8s6kf8.jpg", "8exb87.jpg",
         "zh6js1.jpg", "ysr9ym.jpg", "z0es0r.jpg", "ykk3lw.jpg", "ycu9wz.jpg", "xk9k4m.jpg", "wwxbjf.jpg",
         "wfz75c.jpg", "vhgf0s.jpg", "u3fvs6.jpg", "tsl7bd.jpg", "84mj9v.jpg", "we5nwp.jpg", "s6qgjc.jpg",
-        "jcror9.jpg", "5uk2ca.jpg", "5cbn0o.jpg", "8dbcuj.jpg"
+        "jcror9.jpg", "5uk2ca.jpg", "5cbn0o.jpg", "8dbcuj.jpg",
+        "76pper.jpg", "p571ek.jpg", "zgble3.jpg", "y7x0hp.jpg", "ppoyu5.jpg", "p3u72i.jpg", "r4atez.jpg",
+        "1ukd3b.jpg", "0jbjc1.jpg", "kgd3ho.jpg", "uzxt6h.jpg", "2guc2p.jpg", "h8db80.jpg"
     ],
     "Gudeg": [
         "3zkmn8.jpg", "fdqm68.jpg", "yhpp0w.jpg", "vts83l.jpg", "oyl7r3.jpg", "mer6lb.jpg", "ja4htw.jpg",
@@ -64,7 +77,8 @@ label_dict = {
         "vyu4s1.jpg", "7xrp8g.jpg", "qpwjg4.jpg", "30f84k.jpg", "y8vfvk.jpg", "wgnpok.jpg", "vqrbrp.jpg",
         "vs2xna.jpg", "vttgn3.jpg", "vwrsky.jpg", "w6yn02.jpg", "uxl04w.jpg", "su8rbx.jpg", "sx8sh7.jpg",
         "s6v0d4.jpg", "qyg63n.jpg", "posxbw.jpg", "pcfzp4.jpg", "pci4oj.jpg", "mv4g7m.jpg", "jls4p0.jpg",
-        "jdrt61.jpg", "jeqyd8.jpg", "v5hi5e.jpg", "oyr468.jpg", "nene62.jpg"
+        "jdrt61.jpg", "jeqyd8.jpg", "v5hi5e.jpg", "oyr468.jpg", "nene62.jpg",
+        "hvqfsy.jpg", "bps3ft.jpg", "jqcuwk.jpg", "zs1yro.jpg", "74eg69.jpg", "6plsyp.jpg", "j2is0l.jpg"
     ],
     "Nasi Goreng": [
         "ywrdwy.jpg", "fg1ax9.jpg", "4ymxbv.jpg", "d8nxuv.jpg", "7swrro.jpg", "jo34e4.jpg", "ie80bn.jpg",
@@ -73,14 +87,18 @@ label_dict = {
         "of2v92.jpg", "robzaw.jpg", "n15gzn.jpg", "k57ok4.jpg", "x7lih8.jpg", "uj8i6c.jpg", "xdeqr5.jpg",
         "zyvkir.jpg", "yp351h.jpg", "z81cp5.jpg", "zinra3.jpg", "19icyo.jpg", "11z4yt.jpg", "0aiagz.jpg",
         "5bsfov.jpg", "2e7igv.jpg", "1v9bz6.jpg", "1dtrgj.jpg", "0i9h44.jpg", "0i1gxc.jpg", "0cczsl.jpg",
-        "0bh6sh.jpg", "00sv9l.jpg", "4427yo.jpg", "885f7b.jpg", "se72k1.jpg", "48ms9j.jpg"
+        "0bh6sh.jpg", "00sv9l.jpg", "4427yo.jpg", "885f7b.jpg", "se72k1.jpg", "48ms9j.jpg",
+        "hdpzi4.jpg", "ovqh9m.jpg", "t5a3bl.jpg", "gyb85p.jpg", "0n1i0k.jpg", "ww2b9c.jpg", "zbicqn.jpg",
+        "lrud0n.jpg", "y30bmc.jpg"
     ],
     "Pempek": [
         "5bjuxb.jpg", "jpoh5g.jpg", "l76w1a.jpg", "r36pzw.jpg", "wqjqj6.jpg", "hhhgmp.jpg", "a7ovsa.jpg",
         "b13dqn.jpg", "p0vlh8.jpg", "9kvvqn.jpg", "jgfc52.jpg", "nu7wmm.jpg", "thrv5n.jpg", "eef3pb.jpg",
         "bq2r49.jpg", "ye59oz.jpg", "ypg62x.jpg", "zprbi0.jpg", "xg9068.jpg", "ycxjkl.jpg", "wq8hfg.jpg",
         "qe20dk.jpg", "915nvv.jpg", "9whi9y.jpg", "8x53sf.jpg", "7ufolm.jpg", "5bogwu.jpg", "4mc4wr.jpg",
-        "4kfq4b.jpg", "3yrmom.jpg", "99856s.jpg", "9w4enl.jpg", "9resvi.jpg", "7mrby1.jpg", "7c0l6c.jpg"
+        "4kfq4b.jpg", "3yrmom.jpg", "99856s.jpg", "9w4enl.jpg", "9resvi.jpg", "7mrby1.jpg", "7c0l6c.jpg",
+        "uz3ute.jpg", "8t6epr.jpg", "rzdst5.jpg", "h682ic.jpg", "9p1zjf.jpg", "eesc7w.jpg", "uubpy3.jpg",
+        "b8qa6s.jpg", "q6heuu.jpg"
     ],
     "Rawon": [
         "tl3xrw.jpg", "mg0gmg.jpg", "o343bz.jpg", "qzamp6.jpg", "3mj09q.jpg", "li3p9g.jpg", "w8ciua.jpg",
@@ -88,7 +106,9 @@ label_dict = {
         "m23fdz.jpg", "j736gc.jpg", "gjrko1.jpg", "glfwz1.jpg", "9d0hyy.jpg", "zp5smb.jpg", "vq96ba.jpg",
         "y13sh9.jpg", "xaibrr.jpg", "xf8pf8.jpg", "xe09c4.jpg", "ygd37o.jpg", "3w3poi.jpg", "68mly1.jpg",
         "8zaug5.jpg", "a6ovv0.jpg", "06ni3h.jpg", "6ualqk.jpg", "bx0fdi.jpg", "bpd5rj.jpg", "b7z0dj.jpg",
-        "hkpad9.jpg", "epek9v.jpg", "emdrz5.jpg", "m1k2x8.jpg"
+        "hkpad9.jpg", "epek9v.jpg", "emdrz5.jpg", "m1k2x8.jpg",
+        "zkxdrp.jpg", "2otxod.jpg", "09iyo8.jpg", "f61m2v.jpg", "w1gfy0.jpg", "egwn1m.jpg", "2vopjg.jpg",
+        "tvzrff.jpg", "ldeaat.jpg"
     ],
     "Rendang": [
         "tshgb1.jpg", "oa6s97.jpg", "t3xubf.jpg", "bcyieq.jpg", "79034h.jpg", "a4rimo.jpg", "rtpav4.jpg",
@@ -96,26 +116,32 @@ label_dict = {
         "g01hd6.jpg", "uw8jaq.jpg", "ox0yiz.jpg", "x2ngtm.jpg", "j3cj82.jpg", "0n53jf.jpg", "r1dwjr.jpg",
         "n7nm4v.jpg", "oyiy5b.jpg", "tqw5kn.jpg", "tklbvc.jpg", "q7izrp.jpg", "treiw0.jpg", "5596vl.jpg",
         "9hlfxr.jpg", "8harg6.jpg", "7sdrcv.jpg", "230e1n.jpg", "29u1i4.jpg", "05rl1p.jpg", "2g9yhj.jpg",
-        "1z746f.jpg", "1ws386.jpg", "5koy6m.jpg", "2jynpg.jpg", "3c68du.jpg", "gf8act.jpg", "l5yf76.jpg", "baqkam.jpg"
+        "1z746f.jpg", "1ws386.jpg", "5koy6m.jpg", "2jynpg.jpg", "3c68du.jpg", "gf8act.jpg", "l5yf76.jpg", "baqkam.jpg",
+        "g3g2vr.jpg", "pbueiu.jpg", "y56hok.jpg", "2ci3qb.jpg", "oqow4c.jpg", "upqrty.jpg", "9u0eri.jpg",
+        "02y5fc.jpg", "9w86ut.jpg", "nthzrp.jpg"
     ],
     "Sate Madura": [
         "8setq1.jpg", "1tixag.jpg", "zggq0o.jpg", "u2aviq.jpg", "oza4lj.jpg", "wiytaw.jpg", "7ku8dd.jpg", "20wnhk.jpg",
         "t2rvsa.jpg", "souf92.jpg", "rxjuqb.jpg", "sj0bdq.jpg", "sfdgq3.jpg", "sabp9v.jpg", "ru4er4.jpg", "073u7f.jpg",
         "17zxr8.jpg", "xkgods.jpg", "5ytesw.jpg", "274329.jpg", "044hgc.jpg", "134ovu.jpg", "27h8hs.jpg", "40ma7s.jpg",
-        "38ccq9.jpg", "30v05m.jpg", "5u89p9.jpg", "4voy9c.jpg"
+        "38ccq9.jpg", "30v05m.jpg", "5u89p9.jpg", "4voy9c.jpg",
+        "akbl5o.jpg", "bu9thb.jpg", "47kgfi.jpg"
     ],
     "Sate Padang": [
         "m95dai.jpg", "mdktzf.jpg", "d2d8x7.jpg", "7kbp43.jpg", "b0bo3t.jpg", "lkadmu.jpg", "qb6cjs.jpg",
         "nywhe8.jpg", "29vvh7.jpg", "x6wbt7.jpg", "io72zl.jpg", "jjpnkj.jpg", "jmgp9d.jpg", "i2v6f7.jpg",
         "ibb7np.jpg", "oaaxob.jpg", "pu3gos.jpg", "0us23t.jpg", "0ulszd.jpg", "0pnjze.jpg", "9xwtq3.jpg",
         "19tv5c.jpg", "400eom.jpg", "5bchh9.jpg", "846gqq.jpg", "80evge.jpg", "73wypf.jpg", "8xwlpp.jpg",
-        "8he9j8.jpg", "7pjfqb.jpg"
+        "8he9j8.jpg", "7pjfqb.jpg",
+        "l90ivd.jpg", "s3aw39.jpg", "df93nb.jpg", "ufxvv9.jpg", "2vdc6c.jpg", "f73jrf.jpg", "muh30p.jpg"
     ],
     "Soto": [
         "qfnqdh.jpg", "g4rc8o.jpg", "bnyf4s.jpg", "cb3je6.jpg", "y2wlt3.jpg", "b32amx.jpg", "uz8vpn.jpg",
         "zar2dz.jpg", "3kx7k0.jpg", "sbo0me.jpg", "gvsvqv.jpg", "nph7w2.jpg", "l89ykz.jpg", "jwg6in.jpg", "15xaty.jpg",
         "dlt2qa.jpg", "a44jun.jpg", "ajy0b6.jpg", "g52zkp.jpg", "ga5p62.jpg", "h0plhm.jpg", "hi7ohy.jpg", "6tu7qi.jpg",
         "bxm43j.jpg", "j6jy3n.jpg", "py8rv7.jpg", "6ln0r0.jpg", "c8j0xd.jpg", "gltm2j.jpg", "ggve3n.jpg", "ke5qc7.jpg",
-        "k68gfn.jpg", "iqonfz.jpg", "i1pnbe.jpg", "e4dotv.jpg", "lpuwvq.jpg", "mhxypc.jpg"
+        "k68gfn.jpg", "iqonfz.jpg", "i1pnbe.jpg", "e4dotv.jpg", "lpuwvq.jpg", "mhxypc.jpg",
+        "3inm6s.jpg", "lc6t8c.jpg", "gbz6u6.jpg", "r8gcn2.jpg", "t4m1fa.jpg", "vc8rzd.jpg", "75j885.jpg",
+        "pwmavb.jpg", "cc2pm4.jpg", "kxiwne.jpg", "bovqd5.jpg", "2zrnun.jpg"
     ]
 }
