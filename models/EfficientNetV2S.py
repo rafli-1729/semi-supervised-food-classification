84235d8cba6fb17c2a4b0f7b79c6333112b7fd49
rafli <rafli.4lyfe@gmail.com>
2025-11-07 23:44:48 +0700

    Implement EfficientNetV2S model with data augmentation
    input size 384px

---
+++ models/EfficientNetV2S.py
@@ -0,0 +1,43 @@
+from tensorflow import keras
+from tensorflow.keras.applications import EfficientNetV2S
+from tensorflow.keras.applications.efficientnet_v2 import preprocess_input
+
+pretrained_effnet = EfficientNetV2S(
+    include_top=False,
+    weights='imagenet',
+    pooling=None,
+    input_shape=(*IMG_SIZE, 3)
+)
+pretrained_effnet.trainable = False
+
+# Input
+inputs = keras.Input(shape=(*IMG_SIZE, 3))
+
+# Augmentasi
+x = layers.RandomFlip('horizontal')(inputs)
+x = layers.RandomRotation(0.1)(x)
+x = layers.RandomZoom(0.1)(x)
+x = layers.RandomContrast(0.1)(x)
+x = layers.RandomBrightness(factor=0.0001)(x)
+x = layers.RandomTranslation(0.1, 0.1)(x)
+x = layers.RandomColorJitter(0.1)(x)
+x = keras_cv.layers.RandomCutout(height_factor=0.5,
+                                 width_factor=0.5)(x)
+
+# Preprocessing
+x = preprocess_input(x)
+
+# Feature extractor
+x = pretrained_effnet(x, training=False)
+x = layers.GlobalAveragePooling2D()(x) 
+
+# Head
+x = layers.Dense(
+    64, activation='relu',
+    kernel_regularizer=regularizers.l2(1e-3)
+)(x)
+x = layers.BatchNormalization()(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(15, activation='softmax', dtype='float32')(x)
+
+model_effnet = keras.Model(inputs, outputs)
