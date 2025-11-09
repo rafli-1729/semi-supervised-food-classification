c6d29aa241623f4baeecfd4a8eba36685fb64a9d
rafli <rafli.4lyfe@gmail.com>
2025-11-09 10:42:21 +0700

    Accuracy: 94.0%

--- models/ConvNextTiny.py
+++ models/ConvNextTiny.py
@@ -16,16 +16,14 @@ pretrained_convnext.trainable = False
 # Input
 inputs = keras.Input(shape=(*IMG_SIZE, 3))
 x = layers.RandomFlip('horizontal')(inputs)
-x = layers.RandomRotation(0.15)(x)
-x = layers.RandomZoom(height_factor=(0.0, 0.3),
-                      width_factor=(0.0, 0.3))(x)
-x = layers.RandomContrast(0.3)(x)
-x = layers.RandomBrightness(factor=0.1,
-                            value_range=(0, 255))(x)
+x = layers.RandomRotation(0.1)(x)
+x = layers.RandomZoom(0.1)(x)
+x = layers.RandomContrast(0.1)(x)
+x = layers.RandomBrightness(factor=0.0001)(x)
 x = layers.RandomTranslation(0.1, 0.1)(x)
-x = layers.RandomColorJitter(0.2)(x)
-x = keras_cv.layers.RandomCutout(height_factor=0.4,
-                                 width_factor=0.4)(x)
+x = layers.RandomColorJitter(0.1)(x)
+x = keras_cv.layers.RandomCutout(height_factor=0.5,
+                                 width_factor=0.5)(x)
 
 # Preprocessing
 x = preprocess_input(x)
@@ -36,17 +34,11 @@ x = layers.GlobalAveragePooling2D()(x)
 
 # Head
 x = layers.Dense(
-    256, activation='swish',
-    kernel_regularizer=regularizers.l2(1e-4)
-)(x)
-x = layers.BatchNormalization()(x)
-x = layers.Dropout(0.5)(x)
-x = layers.Dense(
-    128, activation='gelu',
-    kernel_regularizer=regularizers.l2(1e-4)
+    64, activation='relu',
+    kernel_regularizer=regularizers.l2(1e-3)
 )(x)
 x = layers.BatchNormalization()(x)
 x = layers.Dropout(0.5)(x)
 outputs = layers.Dense(15, activation='softmax', dtype='float32')(x)
 
-model_convnext = keras.Model(inputs, outputs)
+model_convnext = keras.Model(inputs, outputs, name = 'convnext_food')
